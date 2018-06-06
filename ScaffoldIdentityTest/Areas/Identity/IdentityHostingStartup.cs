4fb5c768d8a56f0d591a34e4fc265eeffc81b777
Simon Brookes <2011834+smabuk@users.noreply.github.com>
2018-06-06 13:47:07 +0100

    Expected changes

---
+++ ScaffoldIdentityTest/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ScaffoldIdentityTest.Areas.Identity.Data;
+using ScaffoldIdentityTest.Models;
+
+[assembly: HostingStartup(typeof(ScaffoldIdentityTest.Areas.Identity.IdentityHostingStartup))]
+namespace ScaffoldIdentityTest.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ScaffoldIdentityTestContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ScaffoldIdentityTestContextConnection")));
+
+                services.AddDefaultIdentity<ScaffoldIdentityTestUser>()
+                    .AddEntityFrameworkStores<ScaffoldIdentityTestContext>();
+            });
+        }
+    }
+}
