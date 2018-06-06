6ba39dd98ce1aadfdde5fb6403364c9687c32557
Simon Brookes <2011834+smabuk@users.noreply.github.com>
2018-06-06 13:49:18 +0100

    !!! BREAKING CHANGES !!! * Overwrites sitre.css, site.js, favicon1.ico minor overwrites: banner1-3 .svg files

--- ScaffoldIdentityTest/wwwroot/js/site.js
+++ ScaffoldIdentityTest/wwwroot/js/site.js
@@ -1,2 +1,4 @@
-﻿// Customized site.js
-console.log("Customized site.js");
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
